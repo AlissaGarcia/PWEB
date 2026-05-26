fac6f9254171899a948a0d1949658a494dd51577
Alissa Garcia <118315759+AlissaGarcia@users.noreply.github.com>
2026-05-26 17:34:07 +0000

    criação das rotas, subrotas e primeira rota retornando view

--- Aula-05/atividade05/routes/web.php
+++ Aula-05/atividade05/routes/web.php
@@ -6,13 +6,24 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/ola', function () {
+    return 'Olá, Laravel!!!';
+});
+
+Route::get('/curso/ads', function () {
+    return 'Curso de Análise e Desenvolvimeto de Sistemas';
+});
+
+Route::get('/curso/web', function () {
+    return 'Disciplina de Programação Web I';
+});
+
+Route::get('/sobre', function () {
+    return view('sobre');
 });
